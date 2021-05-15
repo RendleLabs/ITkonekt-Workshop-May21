7b9825fef8aee0d74198d7628bbd8faeac708f40
Mark Rendle <mark@markrendle.net>
2021-05-15 12:03:27 +0100

    Decimal

---
+++ src/Ingredients/Protos/DecimalValue.cs
@@ -0,0 +1,20 @@
+// ReSharper disable once CheckNamespace
+namespace Toppings
+{
+    public partial class DecimalValue
+    {
+        private const decimal NanoDivisor = 1_000_000_000m;
+
+        private DecimalValue(decimal value)
+        {
+            Units = (long) decimal.Truncate(value);
+            Nanos = (int) ((value - Units) * NanoDivisor);
+        }
+        
+        public static implicit operator decimal(DecimalValue value) =>
+            value.Units + (value.Nanos / NanoDivisor);
+        
+        public static implicit operator DecimalValue(decimal value) =>
+            new DecimalValue(value);
+    }
+}
