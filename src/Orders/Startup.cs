5866e2c567f6ea97764e127655426b40dcdbaa5e
Mark Rendle <mark@markrendle.net>
2021-05-16 13:18:07 +0100

    orders.yaml

--- src/Orders/Startup.cs
+++ src/Orders/Startup.cs
@@ -24,7 +24,7 @@ namespace Orders
             services.AddGrpcClient<IngredientsService.IngredientsServiceClient>((provider, options) =>
             {
                 var config = provider.GetRequiredService<IConfiguration>();
-                var uri = config.GetServiceUri("Ingredients", "https");
+                var uri = config.GetServiceUri("Ingredients");
                 options.Address = uri ?? new Uri("https://localhost:5003");
             });
 
